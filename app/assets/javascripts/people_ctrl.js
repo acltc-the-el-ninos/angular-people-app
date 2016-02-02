1885e9738ad84f0fd6b7fa0aabbafcba4a2b1982
Al Ilseman <al@ilseman.com>
2016-02-02 11:19:37 -0600

    controller dependencies need to be strings

--- app/assets/javascripts/people_ctrl.js
+++ app/assets/javascripts/people_ctrl.js
@@ -1,7 +1,7 @@
 /* global angular */
 
 (function() {
-  angular.module("app").controller("peopleCtrl", [$scope, $http, function($scope, $http) {
+  angular.module("app").controller("peopleCtrl", ["$scope", "$http", function($scope, $http) {
     $scope.setup = function() {
       $http.get('/api/v1/people.json').then(function(response) {
         $scope.people = response.data;
